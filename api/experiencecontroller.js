49c076fcdd6d7ceff04c021b2a7c84c4481bcf93
parvinder <parvinder75289@gmail.com>
2023-12-12 18:58:23 +0530

    parvinder

--- api/experiencecontroller.js
+++ api/experiencecontroller.js
@@ -1,39 +1,47 @@
+// const { where } = require("sequelize");
 const User = require("../models/usermodels");
 
-
-module.exports ={
- experience:async(req,res)=>{
+module.exports = {
+  experience: async (req, res) => {
     try {
-        const userId = req.body.id;
-        const user = await User.findOne({ where: { id: userId } });
-    
-        if (!user) {
-          return res.status(404).send({
-            message: "User not found",
-            success: false,
-          });
-        }
-    
-        const experiencedata = await user.update({
+      const userId = req.body.id;
+  
+      const [rowCount] = await User.update(
+        {
           experience: req.body.experience,
           description: req.body.description,
           skills: req.body.skills,
-        });
-    
-        console.log("experience updated:", experiencedata);
-    
+        },
+        {
+          where: { id: userId },
+        }
+      );
+  
+      if (rowCount > 0) {
+        // Fetch the updated row
+        const updatedUser = await User.findOne({ where: { id: userId } });
+  
+        console.log("Experience updated:", updatedUser);
+  
         return res.status(200).send({
           message: "Successfully updated",
-          data: experiencedata,
+          data: updatedUser,
           success: true,
         });
-      } catch (error) {
-        console.error("Error updating experience:", error);
-    
-        return res.status(500).send({
-          message: "Internal error",
+      } else {
+        console.log("No rows updated for user with id:", userId);
+        return res.status(404).send({
+          message: "User not found",
           success: false,
         });
       }
- }
-}
+    } catch (error) {
+      console.error("Error updating experience:", error);
+      return res.status(500).send({
+        message: "Internal error",
+        success: false,
+      });
+    }
+  }
+  
+};
